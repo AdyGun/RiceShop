80e9b0b0e02621c734c384bd95f1f738ed3e7e3c
AdyGun <monsieur.vold@gmail.com>
2015-09-14 18:16:11 +0700

    -export empty database

--- database/riceshop.sql
+++ database/riceshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2015 at 08:28 PM
+-- Generation Time: Sep 14, 2015 at 01:13 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -19,8 +19,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `riceshop`
 --
-CREATE DATABASE IF NOT EXISTS `riceshop` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `riceshop`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tdebt`
+--
+
+CREATE TABLE IF NOT EXISTS `tdebt` (
+  `debt_id` varchar(20) NOT NULL,
+  `debt_date` date NOT NULL,
+  `supplier_id` varchar(10) NOT NULL,
+  `user_id` varchar(10) NOT NULL,
+  `debt_description` text NOT NULL,
+  `debt_nominal` int(11) NOT NULL,
+  `debt_capturepath` text NOT NULL,
+  `debt_status` varchar(10) NOT NULL,
+  `debt_deletedate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tdebt`
+--
 
 -- --------------------------------------------------------
 
@@ -30,21 +50,25 @@ USE `riceshop`;
 
 CREATE TABLE IF NOT EXISTS `tlevel_access` (
   `level_id` varchar(10) NOT NULL,
-  `module_id` varchar(10) NOT NULL
+  `module_id` varchar(10) NOT NULL,
+  `access_create` tinyint(1) NOT NULL DEFAULT '1',
+  `access_read` tinyint(1) NOT NULL DEFAULT '1',
+  `access_update` tinyint(1) NOT NULL DEFAULT '1',
+  `access_delete` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Truncate table before insert `tlevel_access`
---
-
-TRUNCATE TABLE `tlevel_access`;
 --
 -- Dumping data for table `tlevel_access`
 --
 
-INSERT INTO `tlevel_access` (`level_id`, `module_id`) VALUES
-('LEV0000', 'MOD0000'),
-('LEV0000', 'MOD0001');
+INSERT INTO `tlevel_access` (`level_id`, `module_id`, `access_create`, `access_read`, `access_update`, `access_delete`) VALUES
+('LEV0000', 'MOD0000', 1, 1, 1, 1),
+('LEV0000', 'MOD0001', 0, 0, 0, 0),
+('LEV0000', 'MOD0002', 0, 0, 0, 0),
+('LEV0000', 'MOD0003', 1, 1, 1, 1),
+('LEV0000', 'MOD0004', 1, 1, 1, 1),
+('LEV0000', 'MOD0005', 1, 1, 1, 1),
+('LEV0000', 'MOD0006', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -59,11 +83,7 @@ CREATE TABLE IF NOT EXISTS `tlog` (
   `log_action` varchar(20) NOT NULL,
   `log_date` datetime NOT NULL,
   `user_id` varchar(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
-
---
--- Truncate table before insert `tlog`
---
+) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tlog`
@@ -81,21 +101,42 @@ CREATE TABLE IF NOT EXISTS `tmodule` (
   `module_category` varchar(30) NOT NULL,
   `module_description` varchar(500) DEFAULT NULL,
   `module_pageurl` varchar(100) NOT NULL,
-  `module_issub` int(1) NOT NULL
+  `module_issub` tinyint(1) NOT NULL DEFAULT '0',
+  `module_hascrud` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Truncate table before insert `tmodule`
---
-
-TRUNCATE TABLE `tmodule`;
 --
 -- Dumping data for table `tmodule`
 --
 
-INSERT INTO `tmodule` (`module_id`, `module_name`, `module_category`, `module_description`, `module_pageurl`, `module_issub`) VALUES
-('MOD0000', 'Module', 'Utility', 'Untuk Mengatur Modul-modul Yang Terdapat Pada Aplikasi Ini', 'module.php', 0),
-('MOD0001', 'Dashboard', 'Utama', 'Menampilkan Halaman Utama Setelah Login', 'index.php', 0);
+INSERT INTO `tmodule` (`module_id`, `module_name`, `module_category`, `module_description`, `module_pageurl`, `module_issub`, `module_hascrud`) VALUES
+('MOD0000', 'Module', 'Utility', 'Untuk Mengatur Modul-modul Yang Terdapat Pada Aplikasi Ini', 'module.php', 0, 1),
+('MOD0001', 'Dashboard', 'Utama', 'Menampilkan Halaman Utama Setelah Login', 'index.php', 0, 0),
+('MOD0002', 'Profile', 'Lain-lain', 'Melihat Profile Dan Kegiatan User. Juga Untuk Mengubah Password User.', 'profile.php', 1, 0),
+('MOD0003', 'User', 'Utility', 'Mengatur Data Pengguna Aplikasi Ini.', 'user.php', 0, 1),
+('MOD0004', 'Level User', 'Utility', 'Mengatur Hak Akses User Terhadap Suatu Halaman.', 'user_level.php', 0, 1),
+('MOD0005', 'Supplier', 'Master', 'Mengatur Data Supplier', 'supplier.php', 0, 1),
+('MOD0006', 'Utang', 'Transaksi', 'Transaksi Utang Dari Supplier', 'debt.php', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tsupplier`
+--
+
+CREATE TABLE IF NOT EXISTS `tsupplier` (
+  `supplier_id` varchar(10) NOT NULL,
+  `supplier_name` varchar(30) NOT NULL,
+  `supplier_address` text NOT NULL,
+  `supplier_city` varchar(30) NOT NULL,
+  `supplier_phone` varchar(20) DEFAULT NULL,
+  `supplier_description` text,
+  `supplier_deletedate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tsupplier`
+--
 
 -- --------------------------------------------------------
 
@@ -116,16 +157,11 @@ CREATE TABLE IF NOT EXISTS `tuser` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Truncate table before insert `tuser`
---
-
-TRUNCATE TABLE `tuser`;
---
 -- Dumping data for table `tuser`
 --
 
 INSERT INTO `tuser` (`user_id`, `user_name`, `user_password`, `user_completename`, `level_id`, `user_status`, `module_id`, `user_accessdate`, `user_deletedate`) VALUES
-('USER0000', 'CREATOR', '31c177061c7de43a7a9d01269fb1b4d8caf4cea2', 'Creator', 'LEV0000', 'Online', 'MOD0001', '2015-08-12 01:25:47', NULL);
+('USER0000', 'CREATOR', '67a74306b06d0c01624fe0d0249a570f4d093747', 'Creatorrr', 'LEV0000', 'Online', 'MOD0001', '2015-09-14 18:11:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -141,11 +177,6 @@ CREATE TABLE IF NOT EXISTS `tuser_level` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Truncate table before insert `tuser_level`
---
-
-TRUNCATE TABLE `tuser_level`;
---
 -- Dumping data for table `tuser_level`
 --
 
@@ -155,6 +186,18 @@ INSERT INTO `tuser_level` (`level_id`, `level_name`, `level_description`, `level
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tdebt`
+--
+ALTER TABLE `tdebt`
+  ADD PRIMARY KEY (`debt_id`);
+
+--
+-- Indexes for table `tlevel_access`
+--
+ALTER TABLE `tlevel_access`
+  ADD PRIMARY KEY (`level_id`,`module_id`);
 
 --
 -- Indexes for table `tlog`
@@ -188,7 +231,7 @@ ALTER TABLE `tuser_level`
 -- AUTO_INCREMENT for table `tlog`
 --
 ALTER TABLE `tlog`
-  MODIFY `log_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+  MODIFY `log_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=125;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
